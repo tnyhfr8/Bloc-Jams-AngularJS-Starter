77079f9cdad0af109374222ad99a01a331a63ad6
tnyhfr@gmail.com <Tony@Tonys-MacBook-Air.local>
2017-10-01 13:43:14 -0700

    Assignment 3 Completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,6 +14,10 @@
         .state('album', {
             url: '/album',
             templateUrl: '/templates/album.html'
+        })
+        .state('collection', {
+            url: '/album/collection',
+            templateUrl: '/templates/collection.html'
         });
      }
 
