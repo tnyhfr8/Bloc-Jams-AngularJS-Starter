742e3f683dd5fa7e12f44c08255015a772f7f8f2
tnyhfr@gmail.com <Tony@Tonys-Air.roshanian.local>
2017-10-02 20:20:10 -0700

    Assignment 5 Fix

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,11 +1,11 @@
 (function() {
      function AlbumCtrl() {
        this.albumData = {
-          title: {{ 'The Colors' }},
-          artist: {{ 'Pablo Picasso' }},
-          label: {{ 'Cubism' }},
-          year: {{ '1881' }},
-          albumArtUrl: {{ '/assets/images/album_covers/01.png' }},
+          title: 'The Colors',
+          artist: 'Pablo Picasso',
+          label: 'Cubism',
+          year: '1881',
+          albumArtUrl: '/assets/images/album_covers/01.png',
           songs: [
             { title: 'Blue', duration: '161.71', audioUrl: '/assets/music/blue' },
             { title: 'Green', duration: '103.96', audioUrl: '/assets/music/green' },
