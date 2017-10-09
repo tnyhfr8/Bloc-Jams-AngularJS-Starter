ac6682d41ecb0785cc06546f6e6fcaa1a7e54f4a
tnyhfr@gmail.com <Tony@Tonys-MacBook-Air.local>
2017-10-08 19:11:19 -0700

    Assignment 7 Completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,6 +2,10 @@
      function SongPlayer() {
           var SongPlayer = {};
 
+          /**
+          * @desc Currrent Playing Song
+          * @type {Object}
+          */
           var currentSong = null;
 
           /**
@@ -29,11 +33,20 @@
              currentSong = song;
           };
 
+          /**
+          * @function playSong
+          * @desc Plays new audio file as currentBuzzObject
+          * @param {Object} song
+          */
+          var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+          };
+
           SongPlayer.play = function(song) {
             if (currentSong !== song) {
                setSong(song);
-               currentBuzzObject.play();
-               song.playing = true;
+               playSong(song);
             } else if (currentSong === song) {
                if (currentBuzzObject.isPaused()) {
                    currentBuzzObject.play();
